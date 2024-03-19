fa249fca59f926a5ee1187a322da97d0c4e0f613
Marty42780 <59181926+Marty42780@users.noreply.github.com>
2024-03-19 01:19:17 +0100

    github pages deployment

---
+++ dist/index.4fe8f393.js
@@ -0,0 +1,2 @@
+document.addEventListener("scroll",function(){window.scrollY/innerHeight>.7?document.querySelector(".nav").classList.remove("hide"):document.querySelector(".nav").classList.add("hide")});
+//# sourceMappingURL=index.4fe8f393.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,8 @@
-import $ from "jquery";
-
-// function fetchCurrentView() {
-//   return (window.scrollY / innerHeight).toFixed(0);
-// }
-
-// function scrollToDestination() {
-//   scrollLock = true;
-//   $("HTML, BODY").animate(
-//     {
-//       scrollTop: innerHeight * destination,
-//     },
-//     700
-//   );
-//   setTimeout(function () {
-//     scrollLock = false;
-//     if (process.env.NODE_ENV !== "production") {
-//       console.log("Scrolled to " + destination);
-//     }
-//   }, 700);
-// }
-
-// Scroll initial detection and update
-// document.addEventListener("DOMContentLoaded", function scrollUpdate() {
-//   destination = fetchCurrentView();
-//   if (process.env.NODE_ENV !== "production") {
-//     console.log("Initial view:", destination);
-//   }
-//   scrollLock = false;
-//   document.querySelectorAll("#scroll-down-button").forEach(function (element) {
-//     element.addEventListener("click", function () {
-//       if (!scrollLock) {
-//         destination += 1;
-//         scrollToDestination();
-//       }
-//     });
-//   });
-// });
-
 document.addEventListener("scroll", function () {
-  relativeScroll = window.scrollY / innerHeight;
+  const relativeScroll = window.scrollY / innerHeight;
   if (relativeScroll > 0.7) {
-    $(".nav")[0].classList.remove("hide");
+    document.querySelector(".nav").classList.remove("hide");
   } else {
-    $(".nav")[0].classList.add("hide");
+    document.querySelector(".nav").classList.add("hide");
   }
-//   if (!scrollLock && fetchCurrentView() == destination) {
-//     if (relativeScroll > destination + 0.01) {
-//       destination += 1;
-//       scrollToDestination();
-//     } else if (relativeScroll < destination - 0.01) {
-//       destination -= 1;
-//       scrollToDestination();
-//     }
-//   }
 });
