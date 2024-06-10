a22d0754768134c4218f8b5d932ff5a292b3674b
Marty42780 <59181926+Marty42780@users.noreply.github.com>
2024-06-10 20:22:29 +0200

    :construction: responsive improvments

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,15 @@ document.addEventListener("scroll", function () {
     document.querySelector(".nav").classList.add("hide");
   }
 });
+
+document
+  .querySelector("#video-splash-button")
+  .addEventListener("click", function () {
+    document.querySelector("#video-splash-canva").classList.add("show");
+  });
+
+document
+  .querySelector("#video-splash-canva button")
+  .addEventListener("click", function () {
+    document.querySelector("#video-splash-canva").classList.remove("show");
+  });
